59eb690637e4b6b6aaf0b6b598b3868ef81e24c1
new_user_name <new@email.com>
2019-10-10 10:12:44 +0300

    Initial Commit

---
+++ IMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IMusic
+//
+//  Created by Илья Маркелов on 10.10.2019.
+//  Copyright © 2019 Илья Маркелов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
