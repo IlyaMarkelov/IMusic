5c303d3845e84d3d8cc6ab8711dec80cd42a01d1
IlyaMarkelov <marckellow@icloud.com>
2019-10-10 12:16:40 +0300

    Parsing logic using Decodable

---
+++ IMusic/SearchResponse.swift
@@ -0,0 +1,21 @@
+//
+//  SearchResponse.swift
+//  IMusic
+//
+//  Created by Илья Маркелов on 10.10.2019.
+//  Copyright © 2019 Илья Маркелов. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResponse: Decodable {
+    var resultCount: Int
+    var results: [Track]
+}
+
+struct Track: Decodable {
+    var trackName: String
+    var collectionName: String?
+    var artistName: String
+    var artworkUrl100: String?
+}
--- IMusic/SearchViewController.swift
+++ IMusic/SearchViewController.swift
@@ -16,11 +16,11 @@ struct TrackModel {
 
 
 class SearchViewController: UITableViewController {
+    private var timer: Timer?
     
     let searchController = UISearchController(searchResultsController: nil)
     
-    let tracks = [TrackModel(trackName: "From The Inside", artistName: "Linkin Park"),
-                 TrackModel(trackName: "Numb", artistName: "Linkin Park")]
+    var tracks = [Track]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,17 +56,35 @@ extension SearchViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         //print(searchText)
         
-        let url = "https://itunes.apple.com/search?term=\(searchText)"
-        
-        AF.request(url).responseData { (dataResponse) in
-            if let error = dataResponse.error {
-                print("Error recieved requesting data: \(error.localizedDescription)")
-                return
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
+            let url = "https://itunes.apple.com/search"
+            let parameters = ["term": "\(searchText)",
+                              "limit":"10"]
+            
+            AF.request(url, method: .get, parameters: parameters, encoding: URLEncoding.default, headers: nil).responseData { (dataResponse) in
+                 if let error = dataResponse.error {
+                                     print("Error recieved requesting data: \(error.localizedDescription)")
+                                     return
+                                 }
+                                 
+                                 guard let data = dataResponse.data else {return}
+                                 
+                                 let decoder = JSONDecoder()
+                                 do {
+                                     let objects = try decoder.decode(SearchResponse.self, from: data)
+                                     print("objects: ", objects)
+                                   self.tracks = objects.results
+                                   self.tableView.reloadData()
+                                     
+                                 } catch let jsonError{
+                                    print("Failed to decode JSON", jsonError)
+                                 }
+                
+                //                  let someStrng = String(data: data, encoding: .utf8)
+                //                  print(someStrng ?? "")
             }
             
-            guard let data = dataResponse.data else {return}
-            let someStrng = String(data: data, encoding: .utf8)
-            print(someStrng ?? "")
-        }
+        })
     }
 }
