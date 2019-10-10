2fafecd3f8b6237b4589f894c8f6de534921920a
IlyaMarkelov <marckellow@icloud.com>
2019-10-10 11:25:04 +0300

    Alomofire and ITunes Search API

--- IMusic/SearchViewController.swift
+++ IMusic/SearchViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 struct TrackModel {
     var trackName: String
@@ -53,6 +54,19 @@ class SearchViewController: UITableViewController {
 
 extension SearchViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print(searchText)
+        //print(searchText)
+        
+        let url = "https://itunes.apple.com/search?term=\(searchText)"
+        
+        AF.request(url).responseData { (dataResponse) in
+            if let error = dataResponse.error {
+                print("Error recieved requesting data: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = dataResponse.data else {return}
+            let someStrng = String(data: data, encoding: .utf8)
+            print(someStrng ?? "")
+        }
     }
 }
