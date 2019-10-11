b9982406d15e9691a5a25b5cfb5deaffe786fbd4
IlyaMarkelov <marckellow@icloud.com>
2019-10-11 13:20:47 +0300

    Track Detail Controller

---
+++ IMusic/Search/TrackDetailView/TrackDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  TrackDetailView.swift
+//  IMusic
+//
+//  Created by Илья Маркелов on 11.10.2019.
+//  Copyright © 2019 Илья Маркелов. All rights reserved.
+//
+
+import UIKit
+
+class TrackDetailView: UIView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
