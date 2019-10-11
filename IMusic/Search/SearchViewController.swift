3abab1b60dbdf83479f799dd4515b80770dab572
IlyaMarkelov <marckellow@icloud.com>
2019-10-11 12:41:17 +0300

    Search Delay Fixing

--- IMusic/Search/SearchViewController.swift
+++ IMusic/Search/SearchViewController.swift
@@ -22,6 +22,8 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
     private var searchViewModel = SearchViewModel.init(cells: [])
     private var timer: Timer?
   
+    
+    private lazy var footerView = FooterView()
   // MARK: Setup
   
   private func setup() {
@@ -62,17 +64,19 @@ class SearchViewController: UIViewController, SearchDisplayLogic {
         
         let nib = UINib(nibName: "TrackCell", bundle: nil)
         table.register(nib, forCellReuseIdentifier: TrackCell.reuseId)
+        table.tableFooterView = footerView
     }
     
   func displayData(viewModel: Search.Model.ViewModel.ViewModelData) {
 
     switch viewModel {
-    case .some:
-        print("ViewController .some")
     case .displayTracks(let searchViewModel):
         print("ViewController .displayTracks")
         self.searchViewModel = searchViewModel
         table.reloadData()
+        footerView.hideLoader()
+    case .displayFooterView:
+        footerView.showLoader()
     }
   }
   
@@ -99,7 +103,22 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 84
     }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.text = "Please enter search term above..."
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        return label
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return searchViewModel.cells.count > 0 ? 0 : 250
+    }
 }
+
+
+//MARK: - UISearchBarDelegate
 
 extension SearchViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
